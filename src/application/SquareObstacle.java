2dc82e2e5b17a0e60a7a46f6fe8846af1ae3b4ca
rohan28-coder <55681001+rohan28-coder@users.noreply.github.com>
2020-12-01 18:01:04 +0530

    Update SquareObstacle.java

--- src/application/SquareObstacle.java
+++ src/application/SquareObstacle.java
@@ -1,1 +1,21 @@
-/begin
+package application;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+import javafx.scene.shape.Shape;
+import javafx.scene.transform.Rotate;
+
+public class SquareObstacle extends Obstacle {
+	
+	
+	
+}
+
+ 
